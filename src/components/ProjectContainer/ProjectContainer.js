03d9b95f403b38d86ef5ae290cc2ab83273a8ad7
Kamaraaj <112767603+Kamaraaj@users.noreply.github.com>
2023-02-02 10:46:03 +0530

    Update ProjectContainer.js

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -11,7 +11,7 @@ function ProjectContainer ( ) {
             img :'https://cdn1.iconfinder.com/data/icons/productivity-aesthetics-vol-2/256/To_Do_List-512.png',
             title : 'Todo-List App',
             description: ' To-Do List project is specially built to keep track of errands or tasks that need to be done. It will be like a task keeper where the user would be able to enter the tasks that they need to do. Once they are done with their tasks they can also remove them from the list by using React Js',
-            link: ''
+            link: 'https://github.com/Kamaraaj/Todo-App'
         },
         
     ]
